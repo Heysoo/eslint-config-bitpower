89fa48b3fde99e5fc779638991146d0696bee37e
Heysoo <hishengs@gmail.com>
2017-09-29 10:48:54 +0800

    add rules;update version to 0.0.2

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports = {
     "no-console": ["error",{
       "allow": ["log", "info", "warn", "error"]
     }],
-    "linebreak-style": ["error", "windows"],
     "padded-blocks": ["off"],
     "no-underscore-dangle": ["off"],
     // "no-underscore-dangle":  ["warn", {
@@ -29,11 +28,32 @@ module.exports = {
     "no-param-reassign": ["off"],
     "func-names": ["warn","never"],
     "brace-style": ["off"],
-    "arrow-body-style": ["error", "always"],
     // prefer
-    "prefer-destructuring": ["warn", {
-      "array": true,
-      "object": true
-    }]
+    // "prefer-destructuring": ["warn", {
+    //   "array": true,
+    //   "object": true
+    // }],
+    "vue/valid-v-if": "error",
+    "linebreak-style": ["off"],
+    "quotes": ["off"],
+    "import/extensions": ["off"],
+    "no-restricted-properties": ["warn"],
+    "eqeqeq": ["warn"],
+    "no-unused-vars": ["warn"],
+    "import/newline-after-import": ["off"],
+    "import/first": ["off"],
+    "import/no-extraneous-dependencies": ["off"],
+    "space-before-function-paren": ["off"],
+    "arrow-parens": ["off"],
+    "object-curly-spacing": ["warn"],
+    "no-multi-spaces": ["off"],
+    "prefer-destructuring": ["off"],
+    "no-debugger": ["off"],
+    "no-lonely-if": ["off"],
+    "no-restricted-syntax": ["off"],
+    "no-shadow": ["off"],
+    "arrow-body-style": ["off"],  // 要求箭头函数体使用大括号
+    "no-else-return": ["off"],    // 禁止 if 语句中 return 语句之后有 else 块
+    "radix": ["off"],             // 强制在parseInt()使用基数参数
   }
 };
